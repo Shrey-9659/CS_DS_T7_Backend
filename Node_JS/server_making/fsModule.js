6babc072172c12b408c8a77bd7c3c569f722e305
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2026-02-27 15:29:22 +0530

    FS module - Lecture

--- Node_JS/server_making/fsModule.js
+++ Node_JS/server_making/fsModule.js
@@ -2,13 +2,51 @@ const http = require("http")
 const fs = require("fs")
 
 const app = http.createServer((req, res) => {
+    if(req.url != "/favicon.ico"){
+    // console.log(fs)
+
     // Writing in a file - Sync
-    fs.writeFileSync("sync.txt", "Writing in a file - sync")
+    // fs.writeFileSync("example.txt", "Writing using Sync Method")
     // Writing in a file - Async
-    fs.writeFile("async.txt", "Writing in a file - Async", (err, data) => {
-        if(err) console.log("File writing failed")
-            else console.log("File writing success")
-    })
+    // fs.writeFile("example.txt", "Writing using Async Method", (err, data) => {
+    //     if(err) console.log(err)
+    //         else console.log("Writing successful...")
+    // })
+
+    // Reading a file - Sync
+    // let readData = fs.readFileSync("example.txt", "utf-8")
+    // console.log(`Sync - ${readData}`)
+
+    // Reading a file - Async
+    // fs.readFile("example.txt", "utf-8", (err, data) => {
+    //     if(err) console.log(err)
+    //         else console.log(`Async - ${data}`)
+    // })
+
+    // Appending in a file - Sync
+    // fs.appendFileSync("example.txt", " -- Appended using Sync Method -- ")
+    // Appending in a file - Async
+    // fs.appendFile("example.txt", " -- Appended using Async Method -- ", (err, data) => {
+    //     if(err) console.log(err)
+    //         else console.log("Appended successful...")
+    // })
+
+    // Copying a file - Sync
+    // fs.cpSync("example.txt", "copyFile.js")
+
+    // Deleting a file - Async
+    // fs.unlink("copyFile.js", (err, data) => {
+    //     if(err) console.log(err)
+    //         else console.log("Deleted successfully...")
+    // })
+
+    // Stats of a file - Sync
+    const statData = fs.statSync("example.txt")
+    console.log(statData)
+
+
+    res.end("OK");
+}
 })
 
 app.listen(3000, () => {
