adbfe84fb1473970e8a21bf278a03908b8a10d43
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2026-04-07 12:23:17 +0530

    Express Project - Display data on submit page

--- ExpressJS/server_01/index.js
+++ ExpressJS/server_01/index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const path = require("path")
+const fs = require("fs")
 
 const HomePage = path.join(__dirname, "/views/HomePage/index.html")
 const FormPage = path.join(__dirname, "/views/RegistrationPage/index.html")
+const SubmitPage = path.join(__dirname, "/views/SubmittedPage/index.html")
 
 const app = express()
 
@@ -26,6 +28,24 @@ app.get("/home/submit", (req, res) => {
 })
 app.post("/home/submit", (req, res) => {
     console.log(req.body)
+    // res.json({
+    //     success : true,
+    //     msg : "User added successfully",
+    //     user : req.body
+    // })
+    fs.readFile(SubmitPage, "utf-8", (err, data) => {
+        if(err) console.log(err)
+            else{
+                // data.replace(oldText, newText)
+                const newData = data
+                .replace("{firstName}", req.body.firstName)
+                .replace("{lastName}", req.body.lastName)
+                .replace("{email}", req.body.email)
+                .replace("{title}", req.body.jobTitle)
+                res.send(newData)
+            }
+    })
+    // res.sendFile(SubmitPage)
 })
 
 app.listen(3000, () => {
